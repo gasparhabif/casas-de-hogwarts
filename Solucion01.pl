024ecdf72dd894fcd47c2f7c5784f996467babaf
Gaspar Habif <gasparhabif@gmail.com>
2020-06-25 16:44:37 -0300

    Added tests

--- Solucion01.pl
+++ Solucion01.pl
@@ -4,10 +4,35 @@
   test(fakeTest):-
     A is 1 + 2,
     A =:= 3.
+  
+  % Test gustos de Jocelyn
+  test(gustoJocelynMollejas, [nondet]) :- 
+    gusto(jocelyn, Comida), 
+    Comida == mollejas.
+  test(gustoJocelynPapas, [nondet]) :- 
+    gusto(jocelyn, Comida), 
+    Comida == papaAlPlomo.
+  test(gustoJocelynProvoleta, [nondet]) :- 
+    gusto(jocelyn, Comida), 
+    Comida == provoleta.
+
+  % Test gustos de Jean Gourmet
+  test(gustoJocelynAsado, [nondet]) :- 
+    gusto(jeanGourmet, Comida), 
+    Comida == asadoDeTira.
+  test(gustoJocelynMollejas, [nondet]) :- 
+    gusto(jeanGourmet, Comida),
+    Comida == mollejas.
+  test(gustoJocelynChinchu, [nondet]) :- 
+    gusto(jeanGourmet, Comida), 
+    Comida == chinchulines.
+  test(gustoJocelynVacio, fail) :- 
+    gusto(jeanGourmet, vacio).
 
 :- end_tests(parrilla).
 
-comida(chinchilines).
+% comida/1
+comida(chinchulines).
 comida(mollejas).
 comida(chorizo).
 comida(morcilla).
@@ -17,12 +42,15 @@ comida(bondiola).
 comida(papaAlPlomo).
 comida(provoleta).
 
+% veggy/1
 veggy(papaAlPlomo).
 veggy(provoleta).
 
+% menuCaro/1
 menuCaro(mollejas).
 menuCaro(chinchulines).
 
+% gusto/2
 % Gustos de Jocelyn
 gusto(jocelyn, mollejas).
 gusto(jocelyn, Plato) :- veggy(Plato).
@@ -38,8 +66,7 @@ desconocido como Falso, por lo tanto al realizarse la consulta: gusto(jeanGourme
 el resultado seria Falso, pues dicho gusto no existe en la base de conocimiento. 
 
 
-Consultas realizadas para conocer los menúes que comerían 
-Jocelyn y Jean Gourmet: 
+Consultas realizadas para conocer los menúes que comerían Jocelyn y Jean Gourmet: 
 
 gusto(jocelyn, Comida). 
 Donde los valores que satisfacen son mollejas, papaAlPlomo y provoleta
